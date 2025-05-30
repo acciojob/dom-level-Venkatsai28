b77a4ef5bf2cf98348c87b305bac238b79dc01d2
Venkatsai28 <123297479+Venkatsai28@users.noreply.github.com>
2025-05-30 21:59:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function getDomLevel(element) {
+  let level = 0;
+
+  while (element) {
+    level++;
+    element = element.parentElement;
+  }
+  return level;
+}
+
+const targetElement = document.getElementById("level");
+
+const domLevel = getDomLevel(targetElement);
+
+alert(`The level of the element is: ${domLevel}`);
